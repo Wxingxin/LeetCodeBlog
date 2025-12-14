b95516af951e0b948a41bff20918b97cd01888a7
WeiJiaXing <weijiaxing765@gmail.com>
2025-12-14 13:22:32 +0800

    12-14

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base:"LeetCodeBlog",
+  base:"/LeetCodeBlog/",
   title: 'My Site',
   icon: '/rspress-icon.png',
   logo: {
